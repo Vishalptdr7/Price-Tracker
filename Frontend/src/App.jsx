db51c7bf8d2bd47f6d4e6ab72637e01d87fcc238
Vishal Patidar <patidarvishal233@gmail.com>
2025-06-01 23:42:28 +0530

    last commited on 1st june

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='color-red-500 text-3xl font-bold underline'>
+        Jai Shree Ram
+      </div>
+        
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+import daisy from "daisyui";
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisy],
+  daisy: {
+    theme: ["light", "dart", "cupcake", "retro"],
+  },
+};
+
+// tailwind.config.js
