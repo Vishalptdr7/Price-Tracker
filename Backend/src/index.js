7bd413725ff4091d9f8b25d31a4f7eb85cdc70a6
Vishal Patidar <patidarvishal233@gmail.com>
2025-07-19 23:24:15 +0530

    commited

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -39,7 +39,7 @@ app.listen(process.env.PORT, () => {
     });
 });
 
-app.get("/", (req, res) => {
+app.get("/api/users/login", (req, res) => {
   res.send("Welcome to Price Tracker API", {
     status: "success",
     message: "Welcome to Price Tracker API",
