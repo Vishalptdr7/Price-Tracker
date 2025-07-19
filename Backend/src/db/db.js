8817df536ae6b9c4a0e44751e25f790640fe9d4b
Vishal Patidar <patidarvishal233@gmail.com>
2025-07-19 16:17:25 +0530

    commited

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -7,9 +7,7 @@ import { startCronJob } from "../cron/priceChecker.js";
 
 const DBconnection = async () => {
   try {
-    const connectionData = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
-    );
+    const connectionData = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`Database Connected: ${connectionData.connection.host}`);
     startCronJob(); // Start the cron job after successful DB connection
   } catch (error) {
