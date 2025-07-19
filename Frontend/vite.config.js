c633502405c76e5b89d21b5b4d9039105d6617b9
Vishal Patidar <patidarvishal233@gmail.com>
2025-07-19 23:43:01 +0530

    commited

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    visualizer({ open: true }), // opens a browser window with bundle stats after build
+  ],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          vendor: ["axios", "zustand", "react-router-dom"],
+        },
+      },
+    },
+  },
+});
