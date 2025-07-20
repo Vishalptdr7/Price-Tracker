f6f2c63cf55d3f302b2e79de8320abdb4d87484c
Vishal Patidar <patidarvishal233@gmail.com>
2025-07-20 19:54:03 +0530

    commitedf

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      
+      "https://price-tracker-frontend-kappa.vercel.app",
       "http://localhost:5173",
     ],
     methods: ["GET", "POST", "DELETE", "PUT", "POST"],
